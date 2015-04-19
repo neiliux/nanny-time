be563ceec3dab6ce07d42efe5242dcb930216578
Neil Breeden III <neil@htmlbuffet.com>
2015-04-19 15:59:04 -0700

    Added support to edit existing data.

--- NannyTime.Api/NannyTime.Api/DAL/NannyTimeDAL.cs
+++ NannyTime.Api/NannyTime.Api/DAL/NannyTimeDAL.cs
@@ -23,12 +23,20 @@ namespace NannyTime.Api.DAL
                 throw new NullReferenceException("Data is invalid");
             }
 
-            document.Root.Add(
-                new XElement("time", 
-                    new XElement("date", timeData.Date),
-                    new XElement("start", timeData.StartTime),
-                    new XElement("end", timeData.StopTime),
-                    new XElement("comment", timeData.Comment)));
+            XElement existingEntry = GetExistingTimeEntry(document, timeData);
+            if (existingEntry != null)
+            {
+                UpdateExistingEntry(existingEntry, timeData);
+            }
+            else
+            {
+                document.Root.Add(
+                    new XElement("time",
+                        new XElement("date", timeData.Date),
+                        new XElement("start", timeData.StartTime),
+                        new XElement("end", timeData.StopTime),
+                        new XElement("comment", timeData.Comment)));
+            }
 
             document.Save(GetPath());
         }
@@ -49,6 +57,42 @@ namespace NannyTime.Api.DAL
                     Date = (DateTime)t.Element("date"),
                     StartTime = (int)t.Element("start"),
                     StopTime = (int)t.Element("end")
+                });
+        }
+
+        private static void UpdateExistingEntry(XElement timeNode, Time time)
+        {
+            var archiveNode = timeNode.Element("archive");
+            if (archiveNode == null)
+            {
+                archiveNode = new XElement("archive");
+                timeNode.Add(archiveNode);
+            }
+
+            archiveNode.Add(new XElement("archiveTime",
+                    new XElement("comment", (string)timeNode.Element("comment")),
+                    new XElement("start", (string)timeNode.Element("start")),
+                    new XElement("end", (string)timeNode.Element("end"))));
+
+            timeNode.SetElementValue("comment", time.Comment);
+            timeNode.SetElementValue("start", time.StartTime);
+            timeNode.SetElementValue("end", time.StopTime);
+        }
+
+        private static XElement GetExistingTimeEntry(XDocument document, Time time)
+        {
+            if (document == null || document.Root == null)
+            {
+                throw new NullReferenceException("document");
+            }
+
+            return document.Root.Descendants("time")
+                .FirstOrDefault(e =>
+                {
+                    var date = (DateTime) e.Element("date");
+                    return time.Date.Day == date.Day &&
+                           time.Date.Month == date.Month &&
+                           time.Date.Year == date.Year;
                 });
         }
 
